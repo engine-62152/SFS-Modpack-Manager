f22cba8612b7a9e3dc37f829f067e60ec6dfdeac
engine-62152 <engine_62152@proton.me>
2026-05-12 18:34:17 +0100

    Update SFS-Mod-Manager.py

--- SFS-Mod-Manager.py
+++ SFS-Mod-Manager.py
@@ -2,7 +2,7 @@
 
 # ========== Configs ========== #
 
-ver = '0.3.1' # Version number
+ver = "0.3.1"  # Version number
 
 # ========== Modules ========== #
 
@@ -10,89 +10,131 @@ import customtkinter, subprocess, os, shutil, pathlib
 
 # ========== Startup =========== #
 
-joint = os.path.expanduser('~'),'\Documents\SFS Mod Manager'
-folder = "".join(joint) # The mod manager folder
+joint = os.path.expanduser("~"), "\Documents\SFS Mod Manager"
+folder = "".join(joint)  # The mod manager folder
 dirprofiles = os.listdir(folder)
-dirprofiles.append('Vanilla')
+dirprofiles.append("Vanilla")
 profiles = dirprofiles
 
 
-if not os.path.isdir(folder) == True: # Creating the folder for mod storage
+if not os.path.isdir(folder) == True:  # Creating the folder for mod storage
     os.mkdir(folder)
-    if not os.path.isdir('C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods'):
-        os.mkdir('C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods')
-    ModdedJoint = folder, 'Modded'
+    if not os.path.isdir(
+        "C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods"
+    ):
+        os.mkdir(
+            "C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods"
+        )
+    ModdedJoint = folder, "Modded"
     ModdedFolder = "/".join(ModdedJoint)
     if not os.path.isdir(ModdedFolder):
         os.mkdir(ModdedFolder)
-        shutil.copytree('C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods', ModdedFolder)
+        shutil.copytree(
+            "C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods",
+            ModdedFolder,
+        )
 else:
-    if not os.path.isdir('C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods'):
-        os.mkdir('C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods')
+    if not os.path.isdir(
+        "C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods"
+    ):
+        os.mkdir(
+            "C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods"
+        )
 
 # ========== Varibles ========== #
 
-option = 'Modded' # Which profile to launch from (profiles don't really exist at the moment)
+option = (
+    "Modded"  # Which profile to launch from (profiles don't really exist at the moment)
+)
 
 # ========== Functions ========== #
 
-def optionmenu_callback(choice): # Dropdown menu
+
+def optionmenu_callback(choice):  # Dropdown menu
     global option
-    if choice == 'Vanilla':
-        option = 'Vanilla'
+    if choice == "Vanilla":
+        option = "Vanilla"
     else:
         option = choice
 
 
-def ToVanilla(): # Works, but not finished 
-    shutil.rmtree('C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods')
+def ToVanilla():  # Works, but not finished
+    shutil.rmtree(
+        "C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods"
+    )
+
 
 def ToModded():
-    profilefold = folder, option, 'Mods'
+    profilefold = folder, option, "Mods"
     profilefolder = "/".join(profilefold)
-    shutil.rmtree('C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods')
-    shutil.copytree(profilefolder,'C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods')
+    shutil.rmtree(
+        "C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods"
+    )
+    shutil.copytree(
+        profilefolder,
+        "C:/Program Files (x86)/Steam/steamapps/common/Spaceflight Simulator/Spaceflight Simulator Game/Mods",
+    )
 
-def launch(): # Launches the game
-    if option == 'Vanilla':
+
+def launch():  # Launches the game
+    if option == "Vanilla":
         ToVanilla()
-        subprocess.Popen(['C:/Program Files (x86)/Steam/steam.exe', '-applaunch', '1718870'])
+        subprocess.Popen(
+            ["C:/Program Files (x86)/Steam/steam.exe", "-applaunch", "1718870"]
+        )
     else:
         ToModded()
-        subprocess.Popen(['C:/Program Files (x86)/Steam/steam.exe', '-applaunch', '1718870'])
+        subprocess.Popen(
+            ["C:/Program Files (x86)/Steam/steam.exe", "-applaunch", "1718870"]
+        )
+
+
 #    LoadBox()
 #    ToVanilla()
 
-def LoadBox(): # Shows a box with a loading message
+
+def LoadBox():  # Shows a box with a loading message
     LoadingBox = customtkinter.CTk()
     LoadingBox.title("Loading...")
     LoadingBox.grid_columnconfigure((0, 1), weight=1)
-    LoadingBox.label = customtkinter.CTkLabel(LoadingBox, text="Launching...", fg_color="transparent")
+    LoadingBox.label = customtkinter.CTkLabel(
+        LoadingBox, text="Launching...", fg_color="transparent"
+    )
     LoadingBox.label.grid(row=1, column=0, padx=0, pady=0, sticky="ew", columnspan=2)
     LoadingBox.mainloop()
+
 
 # ========== GUI code ========== #
 
 app = customtkinter.CTk()
+
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("SFS Mod Manager | "+ver)
+        self.title("SFS Mod Manager | " + ver)
         self.geometry("540x270")
         self.grid_columnconfigure((0), weight=1)
-        self.grid_rowconfigure((2,3), weight=1)
+        self.grid_rowconfigure((2, 3), weight=1)
 
-        self.button = customtkinter.CTkButton(self, text="Launch", command=self.button_callback)
+        self.button = customtkinter.CTkButton(
+            self, text="Launch", command=self.button_callback
+        )
         self.button.grid(row=4, column=0, padx=20, pady=20, sticky="ew")
-        self.label = customtkinter.CTkLabel(self, text="Placeholder", fg_color="transparent")
+        self.label = customtkinter.CTkLabel(
+            self, text="Placeholder", fg_color="transparent"
+        )
         self.label.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
 
-        self.optionmenu = customtkinter.CTkOptionMenu(self, values=profiles,command=optionmenu_callback)
+        self.optionmenu = customtkinter.CTkOptionMenu(
+            self, values=profiles, command=optionmenu_callback
+        )
         self.optionmenu.grid(row=1, column=0, padx=0, pady=0)
-        
+
     def button_callback(self):
         launch()
+
 
 app = App()
 app.mainloop()

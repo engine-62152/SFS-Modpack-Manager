7dba0a2e08fc587561fb2cce8bce6cc585ff18ec
engine-62152 <engine_62152@proton.me>
2026-06-05 20:34:53 +0100

    Fix sfs_dir.txt from being shown as as a profile.

--- SFS-Mod-Manager.py
+++ SFS-Mod-Manager.py
@@ -14,7 +14,7 @@ except ImportError:
     subprocess.run([sys.executable, "-m", "pip", "install", "customtkinter"])
 
 
-VER = "0.3.8"  # Version number
+VER = "0.3.9"  # Version number
 
 os.chdir(os.path.abspath(os.path.dirname(__file__)))  # Fixes permission errors
 
@@ -29,15 +29,10 @@ def WarnBox(title, message):  # Shows a box with a warning
     WarnBox = customtkinter.CTkToplevel()
     WarnBox.title(title)
     WarnBox.grid_columnconfigure((0, 1), weight=1)
-    WarnBox.label = customtkinter.CTkLabel(
-        WarnBox, text=message, fg_color="transparent"
-    )
+    WarnBox.label = customtkinter.CTkLabel(WarnBox, text=message, fg_color="transparent")
     WarnBox.label.grid(row=1, column=0, padx=0, pady=0, sticky="ew", columnspan=2)
-    WarnBox.button = customtkinter.CTkButton(
-        WarnBox, text="Close", command=button_event
-    )
+    WarnBox.button = customtkinter.CTkButton(WarnBox, text="Close", command=button_event)
     WarnBox.button.grid(row=2, column=0, padx=0, pady=0, sticky="ew", columnspan=2)
-
 
 folder = "SFS Mod Manager"  # The mod manager folder
 if not os.path.isdir(folder):  # Creating the folder for mod storage
@@ -46,12 +41,10 @@ if not os.path.isdir(folder):  # Creating the folder for mod storage
     os.mkdir(f"{folder}/Modded/Mods")
 dirprofiles = os.listdir(folder)
 dirprofiles.append("Vanilla")
+dirprofiles.remove("sfs_dir.txt")
 profiles = dirprofiles
 
-if (
-    not os.path.exists(f"{folder}/sfs_dir.txt")
-    or os.path.getsize(f"{folder}/sfs_dir.txt") == 0
-):
+if (not os.path.exists(f"{folder}/sfs_dir.txt") or os.path.getsize(f"{folder}/sfs_dir.txt") == 0):
     filename = customtkinter.filedialog.askdirectory(title="Open SFS Directory")
     SFS_DIR = filename
     with open(f"{folder}/sfs_dir.txt", "w") as f:
@@ -96,10 +89,7 @@ def ToModded():
     profilefold = folder, option, "Mods"
     profilefolder = "/".join(profilefold)
     shutil.rmtree(f"{SFS_DIR}/Mods")
-    shutil.copytree(
-        profilefolder,
-        f"{SFS_DIR}/Mods",
-    )
+    shutil.copytree(profilefolder,f"{SFS_DIR}/Mods",)
 
 
 def launch():  # Launches the game
@@ -121,17 +111,11 @@ class App(customtkinter.CTk):
         self.geometry("540x270")
         self.grid_columnconfigure((0), weight=1)
         self.grid_rowconfigure((2, 3), weight=1)
-        self.button = customtkinter.CTkButton(
-            self, text="Launch", command=self.button_callback
-        )
+        self.button = customtkinter.CTkButton(self, text="Launch", command=self.button_callback)
         self.button.grid(row=4, column=0, padx=20, pady=20, sticky="ew")
-        self.label = customtkinter.CTkLabel(
-            self, text="Placeholder", fg_color="transparent"
-        )
+        self.label = customtkinter.CTkLabel(self, text="Placeholder", fg_color="transparent")
         self.label.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
-        self.optionmenu = customtkinter.CTkOptionMenu(
-            self, values=profiles, command=optionmenu_callback
-        )
+        self.optionmenu = customtkinter.CTkOptionMenu(self, values=profiles, command=optionmenu_callback)
         self.optionmenu.grid(row=1, column=0, padx=0, pady=0)
 
     def button_callback(self):
